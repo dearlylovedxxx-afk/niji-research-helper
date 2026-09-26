b336a9c4f9cc5739f84739f3680bd3f3011456c2
dearlylovedxxx-afk <dearlylovedxxx@gmail.com>
2026-09-27 02:11:50 +0900

    Point pictBLand userscript updates to dedicated toolkit repo

--- PictBLand_Novel_Tools.user.js
+++ PictBLand_Novel_Tools.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         pictBLand 小説TXTツール
 // @namespace    local.pictbland.novel-text-tools
-// @version      0.1.4
+// @version      0.1.5
 // @description  pictBLandの閲覧可能な小説をページ分割して編集・整形し、TXTとしてダウンロード／共有保存します。
 // @match        https://pictbland.net/items/detail/*
 // @run-at       document-idle
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/dearlylovedxxx-afk/niji-research-helper/main/PictBLand_Novel_Tools.meta.js
-// @downloadURL  https://raw.githubusercontent.com/dearlylovedxxx-afk/niji-research-helper/main/PictBLand_Novel_Tools.user.js
+// @updateURL    https://raw.githubusercontent.com/dearlylovedxxx-afk/pictbland-toolkit/main/PictBLand_Novel_Tools.meta.js
+// @downloadURL  https://raw.githubusercontent.com/dearlylovedxxx-afk/pictbland-toolkit/main/PictBLand_Novel_Tools.user.js
 // ==/UserScript==
 
 (() => {
